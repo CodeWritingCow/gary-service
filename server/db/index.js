82a194957f8fef137a7f5179ee85ef862e4ea0fb
Gary Pang <gary.pang@gmail.com>
2019-02-06 18:21:12 -0500

    Write script to generate fake data
    Script saves fake data for "category" and "event" tables to database.
    
    Script saves 100 entries for "event" tables.
    
    TODO:
    In "event" table, fix "price" and "rating" rows to show decimal points.
    
    Fix "image_url" to use random URL of images in repo.
    
    Fix "category" to be only numbers between 1-5 (as foreign keys referencing the "category" table).

--- server/db/index.js
+++ server/db/index.js
@@ -1,3 +1,7 @@
+// To create tables:
+// in command line, run 'sqlite3 events.db'
+// then in sqlite terminal, run '.read db.sql'
+
 const sqlite3 = require('sqlite3').verbose();
 const faker = require('faker');
 const db = new sqlite3.Database('events.db', (err) => {
@@ -7,23 +11,29 @@ const db = new sqlite3.Database('events.db', (err) => {
     console.log('Connected to the SQLite database');
 });
 
-db.serialize(function () {
-    db.run(`DROP TABLE IF EXISTS category;`);
-    db.run(`CREATE TABLE category (name TEXT)`);    
-
-    var statement = db.prepare("INSERT INTO category VALUES (?)");
+db.serialize(function () {    
+    // insert data into category table
+    let statement = db.prepare("INSERT INTO category VALUES (?)");
     let categories = ['Recently Viewed' ,'Cultural Tours', 'Half-day Tours', 'Day Trips'];
 
     categories.forEach((category) => {
         statement.run(category);
     })
-
     statement.finalize();
+
+    // statementTwo VALUES are, in order:
+    // reviews_count; price; rating; name; image_url; category
+    let statementTwo = db.prepare(`INSERT INTO event VALUES(?, ?, ?, ?, ?, ?)`);
     
+    for (let index = 0; index < 100; index++) {
+        statementTwo.run(faker.random.number(), faker.commerce.price(), faker.random.number(), faker.address.country(), faker.image.imageUrl(), faker.random.number());        
+    }
+    statementTwo.finalize();
+
+    // log rows from category table
     db.each("SELECT rowid AS id, name FROM category", function(err, row) {
         console.log(row.id + ": " + row.name);
     });
-
 });
 
 
@@ -33,4 +43,3 @@ db.close((err) => {
     }
     console.log('Closed database connection');
 });
-
