d3e260587609117a27a4545781ebdd82bdf546cc
Gary Pang <gary.pang@gmail.com>
2019-02-06 15:51:03 -0500

    Make 'category' SQL table with script
    Delete MySQL database-connection script. Replace with SQLite3 script for reading and writing to a single-file database.
    
    Script creates a 'category' table in events.db.
    
    NPM install sqlite3 and NPM uninstall mysql.

--- server/db/index.js
+++ server/db/index.js
@@ -1,12 +1,36 @@
-let mysql = require('mysql');
-
-let connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'tripCounselor',
-    password: ''
+const sqlite3 = require('sqlite3').verbose();
+const faker = require('faker');
+const db = new sqlite3.Database('events.db', (err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Connected to the SQLite database');
 });
 
-connection.connect();
+db.serialize(function () {
+    db.run(`DROP TABLE IF EXISTS category;`);
+    db.run(`CREATE TABLE category (name TEXT)`);    
 
-module.exports = connection;
+    var statement = db.prepare("INSERT INTO category VALUES (?)");
+    let categories = ['Recently Viewed' ,'Cultural Tours', 'Half-day Tours', 'Day Trips'];
+
+    categories.forEach((category) => {
+        statement.run(category);
+    })
+
+    statement.finalize();
+    
+    db.each("SELECT rowid AS id, name FROM category", function(err, row) {
+        console.log(row.id + ": " + row.name);
+    });
+
+});
+
+
+db.close((err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Closed database connection');
+});
+
