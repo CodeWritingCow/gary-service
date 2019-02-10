0e5234ea8e4c0a7a2fb3e7af8d7e1beac2f15e1a
Gary Pang <gary.pang@gmail.com>
2019-02-10 15:11:40 -0500

    Rewrite test description
    Previous description was vague. New description is specific.

--- server/db/seed.test.js
+++ server/db/seed.test.js
@@ -4,24 +4,8 @@ const request = require('supertest');
 
 // console.log('app loaded', app);
 
-// test('Mooooooo', () => {
-//   expect(1 + 1).toBe(2)
-// });
-
-describe('Test the API server', () => {
-    // test('should pass integration test', () => {
-    //   request(app)
-    //     .get('/')
-    //     .expect(200, 'moo')
-    //     .end((err) => {
-    //       if (err) throw done(err);
-    //       done();
-    //     });
-    // })
-
-    test('should pass integration tests', () => {
-        // sample data:
-        // [{"reviews_count":24502,"price":980,"rating":2.1,"name":"Papua New Guinea","image_url":"./public/images/01.jpg","category":2}]
+describe('GET endpoint /api/events/:id', () => {
+    test(`should return data based on an event's ID`, () => {
         return request(app)
             .get('/api/events/1')
             .then((res) => {
@@ -32,6 +16,6 @@ describe('Test the API server', () => {
                 expect(typeof res.body[0].name).toBe('string');
                 expect(typeof res.body[0].image_url).toBe('string');
                 expect(typeof res.body[0].category).toBe('number');
-            })
+            });
     });
 });
