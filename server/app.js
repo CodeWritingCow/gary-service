94aadb4872f454d64be28190da0ec2b136f55cf1
Gary Pang <gary.pang@gmail.com>
2019-02-10 15:24:42 -0500

    Uninstall NPM module body-parser
    Express v4+ comes with body-parser JSON and urlencoded. So body-parser is unnecessary for this project.

--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const controller = require('./controllers');
 
 app.use(express.static(__dirname + '/../public'));
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Test route
 app.get('/', (req, res) => {
