90f3d304b43895c3b325aed97a7a1890eebb0ee9
Gary Pang <gary.pang@gmail.com>
2019-02-12 01:14:53 -0500

    Add CSS styles to <Event />
    On hover, the image within <Event /> brightens.

--- client/src/components/Event.jsx
+++ client/src/components/Event.jsx
@@ -5,11 +5,13 @@ function Event ({props}) {
     
     return (
         <div className="event">
-            <img src={props.image_url} alt="Photo" />
-            <a className="name" href="#">{props.name}</a>
+            <img src={props.image_url} alt={props.name} />
             {/* <p>{props.category}</p> */}
-            <p className="info">Rating: {props.rating} {props.reviews_count} Reviews</p>
-            <p className="price">from ${props.price}*</p>
+            <div className="details">
+                <a className="name" href="#">{props.name}</a>
+                <div className="info">Rating: {props.rating} {props.reviews_count} Reviews</div>
+                <div className="price">from <strong>${props.price}*</strong></div>
+            </div>
         </div>
     );
 }
