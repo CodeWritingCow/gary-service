ed94d020f5cdc39736492117342c4b09844f0d59
Gary Pang <gary.pang@gmail.com>
2019-02-12 21:18:55 -0500

    Add ratings icons to event component
    Dynamically render the number of ratings icons based on an event's rating on Trip Advisor.
    
    The icons are CSS-modified versions of the circle icon from the Font Awesome library.
    
    See Trello ticket, "Add ratings icons to event component." https://trello.com/c/l4OeFKjm
    
    TODO: Show half dots for ratings such as 4.5

--- client/src/components/Event.jsx
+++ client/src/components/Event.jsx
@@ -1,16 +1,45 @@
 import React from 'react';
 
-function Event ({props}) {    
+function Event ({props}) {
+    let ratingStyle = {
+        'color': '#00a680',
+        'border': '#00a680 1px solid',
+        'padding': '2px',
+        'borderRadius': '25px',
+        'fontSize': '.5em'
+    }
+
+    let ratingStyleWhite = {
+        'color': 'white',
+        'border': '#00a680 1px solid',
+        'padding': '2px',
+        'borderRadius': '25px',
+        'fontSize': '.5em'
+    }
+
+    let ratings = [];
+    for (let index = 0; index < 5; index++) {
+        if (index < Math.round(props.rating)) { // TODO: Show half dots for ratings such as 4.5
+            ratings.push(0);
+        } else {
+            ratings.push(1);
+        }
+    }
+    // console.log('props.rating is:', props.rating);
+    // console.log(ratings);
+    
     return (
         <div className="event">
-            {/* <img src={props.image_url} alt={props.name} /> */}
             {/* <p>{props.category}</p> */}
             <div className="inner image" style={{ 'backgroundImage': `url("${props.image_url}")` }}></div>
             <i className="far fa-bookmark fa-2x bookmark"></i>
             <div className="details">
                 <a className="name" href="#">{props.name}</a>
-                {/* <i className="far fa-dot-circle" style={{ 'color': 'green'}}></i> */}
-                <div className="info">Rating: {props.rating} {props.reviews_count} Reviews</div>
+                <div className="info">
+                    {/* HARDCODED: {[0,0,0,0,1].map(num => num === 0 ? <i className="fas fa-circle fa-xs" style={ratingStyle}></i> : <i className="fas fa-circle fa-xs" style={ratingStyleWhite}></i>)} {props.reviews_count} Reviews */}
+                    {/* HARDCODED REFACTORED: {[0,0,0,0,1].map(num => <i className="fas fa-circle fa-xs" style={num === 0 ? ratingStyle : ratingStyleWhite}></i>)} {props.reviews_count} Reviews */}
+                    {ratings.map((num, index) => <i className="fas fa-circle fa-xs" style={num === 0 ? ratingStyle : ratingStyleWhite} key={index}></i>)} {props.reviews_count} Reviews
+                </div>
                 <div className="price">from <strong>${props.price.toFixed(2)}*</strong></div>
             </div>
         </div>
