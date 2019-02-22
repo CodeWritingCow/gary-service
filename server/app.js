faca8915b4b11405a5e0768fe26aef37468951bb
Gary Pang <gary.pang@gmail.com>
2019-02-22 15:56:27 -0500

    Enable gzip text compression
    NPM install 'compression,' a middleware for compressing text response with gzip.
    
    Goal is to reduce page load time.

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
+const compression = require('compression');
 const express = require('express');
 const app = express();
 const controller = require('./controllers');
 const cors = require('cors');
 
+app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/../public'));
