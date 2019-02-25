9e3b9f5cfcdf5c0f7830f7f8cd57f249127dc2c8
Gary Pang <gary.pang@gmail.com>
2019-02-25 00:25:00 -0500

    Remove extra / from AWS_URL

--- client/src/config.js
+++ client/src/config.js
@@ -5,6 +5,6 @@ let os = require('os');
 
 export const config = {
     HOSTNAME: os.hostname(),
-    AWS_URL: 'http://18.218.156.11/', // Change IP to your AWS EC2 instance's IPv4 Public IP
+    AWS_URL: 'http://18.218.156.11', // Change IP to your AWS EC2 instance's IPv4 Public IP
     LOCALHOST: 'http://localhost:3016'
 }
